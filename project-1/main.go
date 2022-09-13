eafc82f8c783f891746f0192b61b796ec3f9f128
Ruslan <rthyssen73@gmail.com>
2022-09-13 20:18:24 +0200

    project-1 is ready

--- project-1/main.go
+++ project-1/main.go
@@ -7,7 +7,16 @@ import (
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
+
+type Article struct {
+	Id                     uint16
+	Title, Anons, FullText string
+}
+
+var posts = []Article{}
+var showPost = Article{}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
@@ -16,7 +25,31 @@ func index(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, err.Error())
 	}
 
-	t.ExecuteTemplate(w, "index", nil)
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/golang")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Get data:
+	res, err := db.Query("SELECT * FROM `article`")
+	if err != nil {
+		panic(err)
+	}
+
+	posts = []Article{}
+
+	for res.Next() {
+		var post Article
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+		if err != nil {
+			panic(err)
+		}
+
+		posts = append(posts, post)
+	}
+
+	t.ExecuteTemplate(w, "index", posts)
 }
 
 func create(w http.ResponseWriter, r *http.Request) {
@@ -30,31 +63,74 @@ func create(w http.ResponseWriter, r *http.Request) {
 }
 
 func save_article(w http.ResponseWriter, r *http.Request) {
-	title := r.FormValue("title") // we get name from "name" in "Form" in "input"
+	title := r.FormValue("title") // we get name from "name" in "Form" and "input" fields in file "create.html"
 	anons := r.FormValue("anons")
 	full_text := r.FormValue("full_text")
 
-	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/golang")
+	if title == "" || anons == "" || full_text == "" {
+		fmt.Fprintf(w, "Not all fields are completed")
+	} else {
+		db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/golang")
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+
+		// Data Implementation:
+		insert, err := db.Query(fmt.Sprintf("INSERT INTO `article` (`title`, `anons`, `full_text`) VALUES('%s', '%s','%s')", title, anons, full_text))
+		if err != nil {
+			panic(err)
+		}
+		defer insert.Close()
+
+		http.Redirect(w, r, "/", http.StatusSeeOther) // Go to the main page after implementation
+	}
+}
+
+func show_post(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	t, err := template.ParseFiles("templates/show.html", "templates/header.html", "templates/footer.html")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/golang") // connect to DB
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	// Data Implementation:
-	insert, err := db.Query(fmt.Sprintf("INSERT INTO `article` (`title`, `anons`, `full_text`) VALUES('%s', '%s','%s')", title, anons, full_text))
+	// Get data:
+	res, err := db.Query(fmt.Sprintf("SELECT * FROM `article` WHERE `id` = '%s'", vars["id"]))
 	if err != nil {
 		panic(err)
 	}
-	defer insert.Close()
 
-	http.Redirect(w, r, "/", http.StatusSeeOther)
+	showPost = Article{}
+	for res.Next() {
+		var post Article
+		err = res.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+		if err != nil {
+			panic(err)
+		}
+
+		showPost = post
+	}
+
+	t.ExecuteTemplate(w, "show", showPost)
 }
 
 func handleFunc() {
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/", index).Methods("GET")
+	rtr.HandleFunc("/create", create).Methods("GET")
+	rtr.HandleFunc("/save_article", save_article).Methods("POST")
+	rtr.HandleFunc("/post/{id:[0-9]+}", show_post).Methods("GET")
+
+	http.Handle("/", rtr)                                                                         // Everything will be worked by GORILLA_MAX
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/")))) // Static file connect!
-	http.HandleFunc("/", index)
-	http.HandleFunc("/create", create)
-	http.HandleFunc("/save_article", save_article)
 	http.ListenAndServe(":8080", nil)
 }
 
